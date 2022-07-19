4000a673a369e215b72eb686ce585f09b0a9df40
Arun Kumar Selvaraj <arun@ioak.io>
2022-07-19 16:18:18 +0530

    Added Qr code reader

--- src/components/Page/CheckinPage/CheckinTile.tsx
+++ src/components/Page/CheckinPage/CheckinTile.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory, useParams } from 'react-router';
+import { QrReader } from 'react-qr-reader';
 import { addDays, format } from 'date-fns';
 import {
   faArrowRightFromBracket,
@@ -22,7 +23,6 @@ import EventModel from '../../../model/EventModel';
 import TrackModel from '../../../model/TrackModel';
 import {
   formatDateText,
-  formatDateTime,
   formatDateTimeText,
 } from '../../../components/Lib/DateUtils';
 
@@ -38,8 +38,8 @@ const CheckinTile = (props: Props) => {
   const history = useHistory();
   const dispatch = useDispatch();
 
-  const [participantId, setParticipantId] = useState<string | null>(null);
-  const [eventId, setEventId] = useState<string | null>(null);
+  const [qrData, setQrData] = useState('');
+  const [showQrReader, setShowQrReader] = useState(false);
 
   const [availableTracks, setAvailableTracks] = useState<TrackModel[]>([]);
 
@@ -70,15 +70,16 @@ const CheckinTile = (props: Props) => {
   const cancel = () => history.goBack();
 
   const handleCheckIn = () => {
-    registerIn(
-      props.space,
-      params.eventId,
-      params.participantId,
-      props.track._id || ''
-    ).then((response: any) => {
-      console.log(response);
-      props.handleChange();
-    });
+    // registerIn(
+    //   props.space,
+    //   params.eventId,
+    //   params.participantId,
+    //   props.track._id || ''
+    // ).then((response: any) => {
+    //   console.log(response);
+    //   props.handleChange();
+    // });
+    setShowQrReader(true);
   };
 
   const handleCheckOut = () => {
@@ -98,59 +99,77 @@ const CheckinTile = (props: Props) => {
   }, []);
 
   return (
-    <div
-      className={`checkin-tile checkin-tile--status-${props.track.status} ${
-        props.track.isLocked ? 'checkin-tile--locked' : ''
-      }`}
-    >
-      <div className="checkin-tile__left">
-        <div className="checkin-tile__left__name">{props.track.name}</div>
-        <div className="checkin-tile__left__time">{`${formatDateTimeText(
-          props.track.from
-        )} to ${formatDateTimeText(props.track.to)}`}</div>
-        <div className="checkin-tile__left__description">
-          {props.track.description}
+    <>
+      {showQrReader && (
+        <div className="qr-scan">
+          <QrReader
+            onResult={(result, error) => {
+              if (result) {
+                setQrData(result?.text);
+              }
+
+              if (error) {
+                console.info(error);
+              }
+            }}
+            style={{ width: '100%' }}
+          />
         </div>
-      </div>
-      <div className="checkin-tile__right">
-        {props.track.isLocked && (
-          <>
-            {/* {props.track.status === 'new' && <div>-</div>} */}
-            {props.track.status === 'active' && (
-              <div className="checkin-tile__right--active checkin-tile__right__indicator">
-                <FontAwesomeIcon icon={faCheck} />
+      )}
+      <div
+        className={`checkin-tile checkin-tile--status-${props.track.status} ${
+          props.track.isLocked ? 'checkin-tile--locked' : ''
+        }`}
+      >
+        <div className="checkin-tile__left">
+          <div className="checkin-tile__left__name">{props.track.name}</div>
+          <div className="checkin-tile__left__time">{`${formatDateTimeText(
+            props.track.from
+          )} to ${formatDateTimeText(props.track.to)}`}</div>
+          <div className="checkin-tile__left__description">
+            {props.track.description}
+          </div>
+        </div>
+        <div className="checkin-tile__right">
+          {props.track.isLocked && (
+            <>
+              {/* {props.track.status === 'new' && <div>-</div>} */}
+              {props.track.status === 'active' && (
+                <div className="checkin-tile__right--active checkin-tile__right__indicator">
+                  <FontAwesomeIcon icon={faCheck} />
+                </div>
+              )}
+              {props.track.status === 'closed' && (
+                <div className="checkin-tile__right--active checkin-tile__right__indicator">
+                  <FontAwesomeIcon icon={faCheck} />
+                </div>
+              )}
+            </>
+          )}
+          {!props.track.isLocked &&
+            ['new', 'closed'].includes(props.track.status) && (
+              <div className="checkin-tile__right--active">
+                <button
+                  className="button checkin-tile__right__action"
+                  onClick={handleCheckIn}
+                >
+                  <FontAwesomeIcon icon={faPersonWalkingDashedLineArrowRight} />
+                </button>
               </div>
             )}
-            {props.track.status === 'closed' && (
-              <div className="checkin-tile__right--active checkin-tile__right__indicator">
-                <FontAwesomeIcon icon={faCheck} />
-              </div>
-            )}
-          </>
-        )}
-        {!props.track.isLocked &&
-          ['new', 'closed'].includes(props.track.status) && (
+          {!props.track.isLocked && props.track.status === 'active' && (
             <div className="checkin-tile__right--active">
               <button
                 className="button checkin-tile__right__action"
-                onClick={handleCheckIn}
+                onClick={handleCheckOut}
               >
-                <FontAwesomeIcon icon={faPersonWalkingDashedLineArrowRight} />
+                <FontAwesomeIcon icon={faArrowRightFromBracket} />
               </button>
             </div>
           )}
-        {!props.track.isLocked && props.track.status === 'active' && (
-          <div className="checkin-tile__right--active">
-            <button
-              className="button checkin-tile__right__action"
-              onClick={handleCheckOut}
-            >
-              <FontAwesomeIcon icon={faArrowRightFromBracket} />
-            </button>
-          </div>
-        )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
