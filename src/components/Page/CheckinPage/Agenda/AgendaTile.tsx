416655f172360e37bdd21f976a9c333c09fd306b
Arun Kumar Selvaraj <arun@ioak.io>
2022-08-11 18:56:37 +0530

    Show duration in agenda tile

--- src/components/Page/CheckinPage/Agenda/AgendaTile.tsx
+++ src/components/Page/CheckinPage/Agenda/AgendaTile.tsx
@@ -1,3 +1,5 @@
+import { interval } from 'd3';
+import { intervalToDuration } from 'date-fns';
 import moment from 'moment';
 import React, { useEffect, useState } from 'react';
 import EventModel from 'src/model/EventModel';
@@ -12,6 +14,26 @@ interface Props {
 }
 
 const AgendaTile = (props: Props) => {
+  const [duration, setDuration] = useState<string>('');
+
+  useEffect(() => {
+    const interval = intervalToDuration({
+      start: new Date(props.track.from),
+      end: new Date(props.track.to),
+    });
+    let _duration = '';
+    if (interval.days && interval.days > 0) {
+      _duration += `${interval.days}d`;
+    }
+    if (interval.hours && interval.hours > 0) {
+      _duration += `${_duration ? ' ' : ''}${interval.hours}h`;
+    }
+    if (interval.minutes && interval.minutes > 0) {
+      _duration += `${_duration ? ' ' : ''}${interval.minutes}m`;
+    }
+    setDuration(_duration);
+  }, props.track);
+
   return (
     <div
       className={`button agenda-tile ${
@@ -25,8 +47,12 @@ const AgendaTile = (props: Props) => {
       <div className="agenda-tile__name">{props.track.name}</div>
       <div className="agenda-tile__description">{props.track.description}</div>
       <div className="agenda-tile__time">
-        {moment(props.track.from).format('h:mm a') || '-'}&nbsp;to&nbsp;
-        {moment(props.track.to).format('MMMM Do, h:mm a') || '-'}
+        <div>
+          {moment(props.track.from).format('h:mm a')}
+          {/* {moment(props.track.from).format('h:mm a') || '-'}&nbsp;to&nbsp;
+          {moment(props.track.to).format('MMMM Do, h:mm a') || '-'} */}
+        </div>
+        <div>{duration}</div>
       </div>
     </div>
   );
